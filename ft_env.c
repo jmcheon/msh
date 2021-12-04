5dbbc7d8e680c3f1ca2d11639fb968bd4d62675d
Jung moo CHEON <cjung-mo@e2r12p20.clusters.42paris.fr>
2021-12-04 17:46:20 +0100

    modify ft_env.c

--- ft_env.c
+++ ft_env.c
@@ -6,7 +6,7 @@
 /*   By: cjung-mo <cjung-mo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 17:24:43 by cjung-mo          #+#    #+#             */
-/*   Updated: 2021/11/30 17:24:44 by cjung-mo         ###   ########.fr       */
+/*   Updated: 2021/12/04 16:56:26 by cjung-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,13 @@ int	ft_env(t_minishell *msh)
 	size_t	i;
 
 	i = -1;
+	if (msh->cmd[0].args[1])
+	{
+		print_err_msg("env: ’",
+			msh->cmd[0].args[1], "‘: No such file or directory\n");
+		g_exit_status = 127;
+		return (1);
+	}
 	while (msh->envp[++i])
 	{
 		if (ft_strchr(msh->envp[i], '='))
